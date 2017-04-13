d3344a16b8a3eb1d0f586b5c47439582a4d64a17
Natalie Macias <nramirez787@gmail.com>
2017-04-13 00:26:05 -1000

    functional display of localStorage

--- js/app.js
+++ js/app.js
@@ -9,9 +9,21 @@
     gender: "",
   }
 
-
   //creates object that contains methods for localStorage handling
   var handler = {
+    //saves user inputs as one entry to local storage
+    saveEntry: function (){
+      var inputs = document.querySelectorAll(".tcell");
+        user.id = inputs[0].value;
+        user.name = inputs[1].value;
+        user.address = inputs[2].value;
+        user.email = inputs[3].value;
+        //converts object into JSON and store in localStorage
+        localStorage.setItem("user_" + localStorage.length, JSON.stringify(user));
+        //reloads the page
+        location.reload();
+    },
+
     //clears user input fields on the page
     clearEntry: function(){
       var inputs = document.querySelectorAll(".tcell");
@@ -19,52 +31,38 @@
         inputs[i].value = "";
       };
     },
-    //saves user inputs as one entry to local storage
-    saveEntry: function (){
-      var storageCount = localStorage.length;
-      // console.log('count', storageCount);
-      // console.log('user', user);
-      var inputs = document.querySelectorAll(".tcell");
-        user.id = inputs[0].value;
-        user.name = inputs[1].value;
-        user.address = inputs[2].value;
-        user.email = inputs[3].value;
-        //converts object into JSON and store in localStorage
-        localStorage.setItem("user_" + storageCount, JSON.stringify(user));
-    },
 
+    //displays user entries
     displayEntry: function(){
-      var dataCount = localStorage.length;
-        console.log('dataCount LENGTH', dataCount);
-        if (dataCount > 0){
-          console.log('frooooooogggyyy');
-          var render = "<div border= '1'>";
-          render += "<div>id</div>";
-          for(i = 0; i < dataCount.length; i++){
-            console.log('ppoooooooppp');
-            var key = localStorage.key(i); //Get  the Key
-            var entry = localStorage.getItem(key); //Get Data from Key
-            var data = JSON.parse(user); //Parse the Data back into the object
-            console.log(data, "data here");
-            console.log('poop parse', data);
-            render += "<li>" + data.id + "</li>";
-          }
-          render += "</div>"
-          display_container.innerHTML = render;
+      if (localStorage.length > 0){
+        var render = "<div>";
+        render += "<div id='entry_containter'>Entries:</div>";
+        for(i = 0; i < localStorage.length; i++){
+          var key = localStorage.key(i); //gets the key
+          var entry = localStorage.getItem(key); //gets data from key
+          var data = JSON.parse(entry); //parses data back into object
+          render += "<ul>";
+          render += "<li>" + data.id + "</li>";
+          render += "<li>" + data.name + "</li>";
+          render += "<li>" + data.address + "</li>";
+          render += "<li>" + data.email + "</li>";
+          render += "</ul>"
         }
+      render += "</div>"
+      display_container.innerHTML = render;
+      }
     }
   };
 
   //Save Button Function
   var save = document.getElementById('save');
     save.addEventListener('click', handler.saveEntry);
-    handler.displayEntry();
 
   //Clear User Input Function
   var clear = document.getElementById('clear');
     clear.addEventListener('click', handler.clearEntry);
 
-  // window.onload = function () {
-  //   handler.displayEntry();
-  // };
+  window.onload = function () {
+    handler.displayEntry();
+  };
 })();
