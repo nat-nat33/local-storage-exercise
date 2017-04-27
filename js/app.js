d7264f6f3427ff613c83acf81807421cbfb4e42b
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-04-26 17:01:53 -1000

    reversed engineered your code

--- js/app.js
+++ js/app.js
@@ -1,68 +1,75 @@
-//creates a function that generates a empty object that will be saved to localStorage
-(function(){
-  var user = {
-    id: 0,
-    name: "",
-    address:"",
-    email: "",
-    mobile: "",
-    gender: "",
+window.onload = function () {
+  displayEntry();
+};
+
+var user = {
+  id: 0,
+  name: "",
+  address:"",
+  email: ""
+}
+
+//Save Button Function
+var save = document.getElementById('save');
+save.addEventListener('click', saveEntry);
+
+//Clear User Input Function
+var clear = document.getElementById('clear');
+clear.addEventListener('click', clearEntry);
+
+//Clear Local Storage
+var clearStorage = document.getElementById('clear_storage');
+clearStorage.addEventListener('click', clearLocalStorage)
+
+//Function that clears local storage
+function clearLocalStorage() {
+  window.localStorage.clear();
+  location.reload();
+}
+
+//Function that saves user inputs as one entry to local storage
+function saveEntry() {
+
+  if(user.id || user.name || user.address || user.email === "") {
+    alert("Please fill in all fields");
   }
 
-  //creates object that contains methods for localStorage handling
-  var handler = {
-    //saves user inputs as one entry to local storage
-    saveEntry: function (){
-      var inputs = document.querySelectorAll(".tcell");
-        user.id = inputs[0].value;
-        user.name = inputs[1].value;
-        user.address = inputs[2].value;
-        user.email = inputs[3].value;
-        //converts object into JSON and store in localStorage
-        localStorage.setItem("user_" + localStorage.length, JSON.stringify(user));
-        //reloads the page
-        location.reload();
-    },
+  var inputs = document.querySelectorAll(".tcell");
+  user.id = inputs[0].value;
+  user.name = inputs[1].value;
+  user.address = inputs[2].value;
+  user.email = inputs[3].value;
+  //converts object into JSON and store in localStorage
+  localStorage.setItem("user_" + localStorage.length, JSON.stringify(user));
+  //reloads the page
+  location.reload();
+}
 
-    //clears user input fields on the page
-    clearEntry: function(){
-      var inputs = document.querySelectorAll(".tcell");
-      for(i = 0; i< inputs.length; i++){
-        inputs[i].value = "";
-      };
-    },
+//Function to clear user input fields on the page
+function clearEntry() {
+  var inputs = document.querySelectorAll(".tcell");
+  for(i = 0; i < inputs.length; i++){
+    inputs[i].value = "";
+  };
+}
 
-    //displays user entries
-    displayEntry: function(){
-      if (localStorage.length > 0){
-        var render = "<div>";
-        render += "<div id='entry_containter'>Entries:</div>";
-        for(i = 0; i < localStorage.length; i++){
-          var key = localStorage.key(i); //gets the key
-          var entry = localStorage.getItem(key); //gets data from key
-          var data = JSON.parse(entry); //parses data back into object
-          render += "<ul>";
-          render += "<li>" + data.id + "</li>";
-          render += "<li>" + data.name + "</li>";
-          render += "<li>" + data.address + "</li>";
-          render += "<li>" + data.email + "</li>";
-          render += "</ul>"
-        }
-      render += "</div>"
-      display_container.innerHTML = render;
-      }
+//Function to display user entries
+function displayEntry(){
+  if (localStorage.length > 0){
+    var render = "<div>";
+    render += "<div id='entry_containter'>Entries:</div>";
+    for(i = 0; i < localStorage.length; i++){
+      var key = localStorage.key(i); //gets the key
+      var entry = localStorage.getItem(key); //gets data from key
+      var data = JSON.parse(entry); //parses data back into object
+      render += "<ul>";
+      render += "<li>" + data.id + "</li>";
+      render += "<li>" + data.name + "</li>";
+      render += "<li>" + data.address + "</li>";
+      render += "<li>" + data.email + "</li>";
+      render += "</ul>"
     }
-  };
-
-  //Save Button Function
-  var save = document.getElementById('save');
-    save.addEventListener('click', handler.saveEntry);
-
-  //Clear User Input Function
-  var clear = document.getElementById('clear');
-    clear.addEventListener('click', handler.clearEntry);
-
-  window.onload = function () {
-    handler.displayEntry();
-  };
-})();
+  render += "</div>"
+  display_container.innerHTML = render;
+  }
+}
